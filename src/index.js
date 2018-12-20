848b108a6b9153c04fb927c508593ebed88eb0fa
ws903 <ws903@nyu.edu>
2018-12-20 12:47:40 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 const Home = () => {
   return (
@@ -13,3 +14,10 @@ ReactDOM.render(
   <Home />,
   document.getElementById('root')
 );
+
+ReactDOM.render((
+	<Router>
+		<Route path="/" render={Home} />
+	</Router>),
+	document.getElementById('root')
+)
