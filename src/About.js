4ac819f6dea723097e2993c30ae3dc5c43b0785a
said shah <saidmysr20@aol.com>
2018-12-19 19:32:44 -0500

    Done.

---
+++ src/About.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, NavLink  } from 'react-router-dom';
+
+class About extends Component {
+
+  render() {
+    return (
+      <div>
+        <h2> this is the about section</h2>
+      </div>
+    );
+  }
+
+}
+
+export default About;
